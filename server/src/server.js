344fcac9705027c691ed1811c3fa4e93dfdef30f
devShubham <web1@decostyle.co.in>
2026-04-21 13:05:22 +0530

    add port

--- server/src/server.js
+++ server/src/server.js
@@ -30,6 +30,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
+const port = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
